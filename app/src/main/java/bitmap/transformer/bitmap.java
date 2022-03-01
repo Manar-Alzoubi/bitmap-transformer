438bb00d6d06c2d73825654f48fb222f5f2c7bd3
Manar-Alzoubi <mzzo3bi20988@gmail.com>
2022-03-01 23:40:49 +0300

    edit

---
+++ app/src/main/java/bitmap/transformer/bitmap.java
@@ -0,0 +1,21 @@
+package bitmap.transformer;
+
+import java.awt.image.BufferedImage;
+
+public class bitmap {
+
+
+//    public static void blackAndWhite(int width, int height) {
+//        int rgb;
+//
+//        for (int h = 1; h < height; h++) {
+//            for (int w = 1; w < width; w++) {
+//                BufferedImage img;
+//                rgb = (img.getRGB(w, h) * 2) % 256;
+//                img.setRGB(w, h, rgb);
+//            }
+//        }
+//    }
+
+}
+
